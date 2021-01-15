630c9da1e7e4b6c5bff9fc53b483543510d6cb33
MuhammadHanzala980 <muhammadhanzala980@gmail.com>
2021-01-15 20:12:33 +0500

    updated

--- Routes/mailtoadmin.js
+++ Routes/mailtoadmin.js
@@ -2,42 +2,63 @@ const express = require('express')
 const app = express.Router()
 const nodemailer = require('nodemailer')
 const smtpPool = require('nodemailer-smtp-pool');
+const { googleapis, google } = require('googleapis')
+
 
 require('dotenv').config()
 app.post('/mailtoadmin', async (req, res) => {
-    try {
-        let transporter = nodemailer.createTransport(smtpPool({
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            auth: {
-                type: 'OAuth2',
-                user:'service64test@gmail.com',
-                pass: 'testforService64',
-            }
-        }))
-        let mailOptions = {
-            from:'"Service64" <service64test@gmail.com>',
-            to: 'service64bd@gmail.com',
-            subject: 'Testing and sendmail',
-            text: 'For clients with plaintext support only',
-            html: '<h3> Name: ' + req.body.name + '</h3>' + '<h4> Email: ' + req.body.email + '</h4>' + '<p> Message: ' + req.body.message + '</p>',
-        }
+    const CLIENT_ID = '71728934820-75nud3o9qbi1d1rrqpmth7q8t259ih2l.apps.googleusercontent.com'
+    const CLIENT_SECRET = 'lYE6VCLEFcx-KM83-gGidrMG'
+    const REDIRECT_URI = 'https://developers.google.com/oauthplayground'
+    const REFRESH_TOKEN = '1//04C9S5jWp8u1MCgYIARAAGAQSNwF-L9IruoSaAcq3G0hVpLtLyQNRM1tr3nzxff8Yd-0PwSZZevCvlZBrzo7-FHTpQuLa_rjgLHU'
+    const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
 
-        await transporter.sendMail(mailOptions, function (err, data) {
-            if (err) {
-                res.send({
-                    message: 'Not Send',
-                    err: err
-                })
+    oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN })
+    console.log(oAuth2Client);
+
+    try {
+        await oAuth2Client().then((token) => {
+            let transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    type: 'OAuth2',
+                    user: 'service64test@gmail.com',
+                    pass: 'testforService64',
+                    clientId: CLIENT_ID,
+                    clientSecret: CLIENT_SECRET,
+                    refreshToken: REFRESH_TOKEN,
+                    accessToken: accessToken
+                }
+            })
+            let mailOptions = {
+                from: '"Service64" <service64test@gmail.com>',
+                to: 'service64bd@gmail.com',
+                subject: 'Testing and sendmail',
+                text: 'For clients with plaintext support only',
+                html: '<h3> Name: ' + req.body.name + '</h3>' + '<h4> Email: ' + req.body.email + '</h4>' + '<p> Message: ' + req.body.message + '</p>',
             }
-            else {
-                res.send({
-                    message: 'Recvied your message',
-                    data: data
-                })
-            }
+
+            transporter.sendMail(mailOptions, function (err, data) {
+                if (err) {
+                    res.send({
+                        message: 'Not Send',
+                        err: err
+                    })
+                }
+                else {
+                    res.send({
+                        message: 'Recvied your message',
+                        data: data
+                    })
+                }
+            })
+            console.log(transporter);
+            res.send(token)
         })
+            .catch((err) => {
+                res.send({ err })
+            })
+
     }
     catch (err) {
         res.json("Not send !");
