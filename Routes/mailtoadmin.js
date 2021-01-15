51142e9536bed746e89e25ca0e0d9ca262b2c518
MuhammadHanzala980 <muhammadhanzala980@gmail.com>
2021-01-15 21:28:05 +0500

    updated

--- Routes/mailtoadmin.js
+++ Routes/mailtoadmin.js
@@ -1,68 +1,68 @@
 const express = require('express')
 const app = express.Router()
-const nodemailer = require('nodemailer')
-const smtpPool = require('nodemailer-smtp-pool');
-const { googleapis, google } = require('googleapis')
+const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
 
+// These id's and secrets should come from .env file.
+const CLIENT_ID = '708414183048-9t60u5hjo5rallni89g61pmh5nhndcdt.apps.googleusercontent.com';
+const CLEINT_SECRET = 'WliuNJPxl3nxMXOip8TnWSA6';
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+const REFRESH_TOKEN = '1//04sVrNYco17NyCgYIARAAGAQSNwF-L9Ir_1vr23DCrO5-uyEean_XjIsTWqMP65qEFXQLR702DhOku_4un8TYOjB_tyesy1VEp9s'
 
-require('dotenv').config()
-app.post('/mailtoadmin', async (req, res) => {
-    const CLIENT_ID = '71728934820-75nud3o9qbi1d1rrqpmth7q8t259ih2l.apps.googleusercontent.com'
-    const CLIENT_SECRET = 'lYE6VCLEFcx-KM83-gGidrMG'
-    const REDIRECT_URI = 'https://developers.google.com/oauthplayground'
-    const REFRESH_TOKEN = '1//04C9S5jWp8u1MCgYIARAAGAQSNwF-L9IruoSaAcq3G0hVpLtLyQNRM1tr3nzxff8Yd-0PwSZZevCvlZBrzo7-FHTpQuLa_rjgLHU'
-    const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
+const oAuth2Client = new google.auth.OAuth2(
+    CLIENT_ID,
+    CLEINT_SECRET,
+    REDIRECT_URI
+);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
-    oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN })
-    console.log(oAuth2Client);
-
+async function sendMail(req) {
     try {
-        await oAuth2Client().then((token) => {
-            let transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                    type: 'OAuth2',
-                    user: 'service64test@gmail.com',
-                    pass: 'testforService64',
-                    clientId: CLIENT_ID,
-                    clientSecret: CLIENT_SECRET,
-                    refreshToken: REFRESH_TOKEN,
-                    accessToken: accessToken
-                }
-            })
-            let mailOptions = {
-                from: '"Service64" <service64test@gmail.com>',
-                to: 'service64bd@gmail.com',
-                subject: 'Testing and sendmail',
-                text: 'For clients with plaintext support only',
-                html: '<h3> Name: ' + req.body.name + '</h3>' + '<h4> Email: ' + req.body.email + '</h4>' + '<p> Message: ' + req.body.message + '</p>',
-            }
+        const accessToken = await oAuth2Client.getAccessToken();
+        const transport = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'OAuth2',
+                user: 'service64test@gmail.com',
+                clientId: CLIENT_ID,
+                clientSecret: CLEINT_SECRET,
+                refreshToken: REFRESH_TOKEN,
+                accessToken: accessToken,
+            },
+        });
 
-            transporter.sendMail(mailOptions, function (err, data) {
-                if (err) {
-                    res.send({
-                        message: 'Not Send',
-                        err: err
-                    })
-                }
-                else {
-                    res.send({
-                        message: 'Recvied your message',
-                        data: data
-                    })
-                }
+        let mailOptions = {
+            from: '"Service64" <service64test@gmail.com>',
+            to: 'muhammadhanzala980@gmail.com',
+            subject: 'Testing and sendmail',
+            text: 'For clients with plaintext support only',
+            html: '<h3> Name: ' + req.body.name + '</h3>' + '<h4> Email: ' + req.body.email + '</h4>' + '<p> Message: ' + req.body.message + '</p>',
+        }
+
+        const result = await transport.sendMail(mailOptions);
+        return result;
+    } catch (error) {
+        return error;
+    }
+}
+
+
+
+
+
+app.post('/mailtoadmin', async (req, res) => {
+    await sendMail(req)
+        .then((result) => {
+            res.send({
+                data: result,
             })
-            console.log(transporter);
-            res.send(token)
         })
-            .catch((err) => {
-                res.send({ err })
+        .catch((error) => {
+            res.send({
+                message: 'Error',
+                data: error
             })
-
-    }
-    catch (err) {
-        res.json("Not send !");
-    }
+        })
 })
 
 module.exports = app
